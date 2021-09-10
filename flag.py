da5b06116c6e6e76b32a84197b3888714a04293f
Dmitriy <warstream55@gmail.com>
2021-09-10 20:44:41 +0300

    Обновил коммент и полумесяц, пофиксил цвета

--- flag.py
+++ flag.py
@@ -6,41 +6,44 @@ from math import cos, sin
 
 
 def show():
-    glClearColor(0.0, 0.25, 0.1, 1.0)  # белый цвет на экране
+    glClearColor(0.0, 0.25, 0.1, 1.0)  # зелёный цвет на экране
     glClear(GL_COLOR_BUFFER_BIT)
 
     glLineWidth(3)
     glBegin(GL_QUADS)
+    glColor3f(1.0, 1.0, 1.0)
     quad_vertexes = [(-1, -1), (-1, 1), (-0.5, 1), (-0.5, -1)]
     for vertex in quad_vertexes:
         glVertex2d(*vertex)
     glEnd()
-    """
+
     glBegin(GL_TRIANGLE_FAN)
-    v = 100
-    glVertex2d(0.5, 0.1)
+    v = 10000
+    glVertex2d(0.1, -0.1)
     for i in range(v):
         a = i / v * 3.1415 * 2
-        glVertex2d(0.1 + 0.5 * cos(a), 0.1 + 0.5 * sin(a))
+        glVertex2d(0.1 + 0.5 * cos(a), -0.1 + 0.5 * sin(a))
     glEnd()
+
+    glBegin(GL_POLYGON)
+    star_coords = []
+    for i in range(11):
+        star_coords.append(())
+    glEnd()
+
     glBegin(GL_TRIANGLE_FAN)
     glColor3f(0.0, 0.25, 0.1)
-    glVertex2d(0, 0)
+    glVertex2d(0.1, -0.1)
     for i in range(v):
         a = i / v * 3.1415 * 2
-        glVertex2d(0 + 0.4 * cos(a), 0 + 0.4 * sin(a))
+        glVertex2d(0.2 + 0.5 * cos(a), 0 + 0.5 * sin(a))
     glEnd()
-    """
-    glBegin(GL_POLYGON)
-
-    glEnd()
-
     glutSwapBuffers()
 
 
 glutInit(sys.argv)
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-glutInitWindowSize(1000, 1000)
+glutInitWindowSize(700, 700)
 glutInitWindowPosition(50, 50)
 window = glutCreateWindow("Pakistan flag")
 glutDisplayFunc(show)
