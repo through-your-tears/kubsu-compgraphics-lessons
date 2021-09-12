886bb0e736eca07a2c54c9f1341b9ea70d339389
Dmitri <warstream55@gmail.com>
2021-09-12 22:17:13 +0300

    Рефактор, звезды всё ещё нет

--- flag.py
+++ flag.py
@@ -6,19 +6,19 @@ from math import cos, sin
 
 
 def show():
-    glClearColor(0.0, 0.25, 0.1, 1.0)  # зелёный цвет на экране
+    white = (1.0, 1.0, 1.0)
+    green = (0.0, 0.25, 0.1)
+    v = 100000  # чем больше число, тем выше точность прорисовки кругов
+    glClearColor(*green, 1.0)
     glClear(GL_COLOR_BUFFER_BIT)
-
-    glLineWidth(3)
     glBegin(GL_QUADS)
-    glColor3f(1.0, 1.0, 1.0)
+    glColor3f(*white)
     quad_vertexes = [(-1, -1), (-1, 1), (-0.5, 1), (-0.5, -1)]
     for vertex in quad_vertexes:
         glVertex2d(*vertex)
     glEnd()
 
     glBegin(GL_TRIANGLE_FAN)
-    v = 10000
     glVertex2d(0.1, -0.1)
     for i in range(v):
         a = i / v * 3.1415 * 2
@@ -32,8 +32,8 @@ def show():
     glEnd()
 
     glBegin(GL_TRIANGLE_FAN)
-    glColor3f(0.0, 0.25, 0.1)
-    glVertex2d(0.1, -0.1)
+    glColor3f(*green)
+    glVertex2d(0.2, 0)
     for i in range(v):
         a = i / v * 3.1415 * 2
         glVertex2d(0.2 + 0.5 * cos(a), 0 + 0.5 * sin(a))
