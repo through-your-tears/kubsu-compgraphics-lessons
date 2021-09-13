41fab24da56cc98bcde2b9dd532552d36e41844c
Dmitri <warstream55@gmail.com>
2021-09-14 00:37:42 +0300

    Рефактор, звезда есть в комментах, но по центру и не работает с полигоном

--- flag.py
+++ flag.py
@@ -2,42 +2,55 @@
 from OpenGL.GLUT import *
 from OpenGL.GLE import *
 from OpenGL.GL import *
-from math import cos, sin
+from math import cos, sin, pi
+
+
+def getX(r, angle):
+    return r * cos(angle * pi / 180)
+
+
+def getY(r, angle):
+    return r * sin(angle * pi / 180)
+
+
+def draw_circle(x, y, r):
+    v = 100000  # чем больше число, тем выше точность прорисовки круга
+    glBegin(GL_TRIANGLE_FAN)
+    glVertex2d(x, y)
+    for i in range(v):
+        a = i / v * pi * 2
+        glVertex2d(x + r * cos(a), y + r * sin(a))
+    glEnd()
 
 
 def show():
     white = (1.0, 1.0, 1.0)
     green = (0.0, 0.25, 0.1)
-    v = 100000  # чем больше число, тем выше точность прорисовки кругов
     glClearColor(*green, 1.0)
     glClear(GL_COLOR_BUFFER_BIT)
-    glBegin(GL_QUADS)
     glColor3f(*white)
+    glBegin(GL_QUADS)
     quad_vertexes = [(-1, -1), (-1, 1), (-0.5, 1), (-0.5, -1)]
     for vertex in quad_vertexes:
         glVertex2d(*vertex)
     glEnd()
-
-    glBegin(GL_TRIANGLE_FAN)
-    glVertex2d(0.1, -0.1)
-    for i in range(v):
-        a = i / v * 3.1415 * 2
-        glVertex2d(0.1 + 0.5 * cos(a), -0.1 + 0.5 * sin(a))
-    glEnd()
-
+    draw_circle(0.1, -0.1, 0.5)
+    '''
     glBegin(GL_POLYGON)
-    star_coords = []
-    for i in range(11):
-        star_coords.append(())
+    deg1 = 18
+    deg2 = 54
+    rb = 0.5
+    r = 0.2
+    coords = [(0, rb), (getX(r, deg2), getY(r, deg2)), (getX(rb, deg1), getY(rb, deg1)),
+              (getX(r, deg1), -getY(r, deg1)), (getX(rb, deg2), -getY(rb, deg2)), (0, -r),
+              (-getX(rb, deg2), -getY(rb, deg2)),  (-getX(r, deg1), -getY(r, deg1)),
+              (-getX(rb, deg1), getY(rb, deg1)), (-getX(r, deg2), getY(r, deg2))]
+    for coord in coords:
+        glVertex2d(*coord)
     glEnd()
-
-    glBegin(GL_TRIANGLE_FAN)
+    '''
     glColor3f(*green)
-    glVertex2d(0.2, 0)
-    for i in range(v):
-        a = i / v * 3.1415 * 2
-        glVertex2d(0.2 + 0.5 * cos(a), 0 + 0.5 * sin(a))
-    glEnd()
+    draw_circle(0.2, 0, 0.5)
     glutSwapBuffers()
 
 
