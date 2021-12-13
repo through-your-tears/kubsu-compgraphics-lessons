dc675e57b21a554253ec39d8b0ab31a7eda65d24
Dmitri <warstream55@gmail.com>
2021-12-14 01:29:50 +0300

    Работа завершена

--- teapots.py
+++ teapots.py
@@ -1,49 +1,83 @@
 from OpenGL.GLUT import *
 from OpenGL.GLE import *
 from OpenGL.GL import *
-from light import light
+
+
+global xrot
+global yrot
+
+
+def f():
+    global xrot
+    global yrot
+    xrot = 0
+    yrot = 0
 
 
 def rotate(key, x, y):
+    global xrot
+    global yrot
     if key == GLUT_KEY_RIGHT:
-        glRotate(5, 0, 1, 0)
+        xrot += 5
     elif key == GLUT_KEY_LEFT:
-        glRotate(-5, 0, 1, 0)
+        xrot -= 5
     elif key == GLUT_KEY_UP:
-        glRotate(5, 1, 0, 0)
+        yrot += 5
     elif key == GLUT_KEY_DOWN:
-        glRotate(-5, 1, 0, 0)
+        yrot -= 5
+
+
+def light():
+    ambient = (1.0, 1.0, 1.0, 1)
+    lightpos = [0, 1, 0, 0]
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient)
+    glEnable(GL_LIGHTING)
+    glEnable(GL_LIGHT0)
+    glLightfv(GL_LIGHT0, GL_POSITION, lightpos)
 
 
 def show():
+    global xrot
+    global yrot
     white = (1.0, 1.0, 1.0)
     green = (0.0, 1.0, 0.0)
     red = (1.0, 0.0, 0.0)
+    black = (0, 0, 0)
     glClearColor(*white, 1.0)
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glMatrixMode(GL_MODELVIEW)
+    glMatrixMode(GL_PROJECTION)
     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
     glLoadIdentity()
     glTranslatef(-0.5, 0, 0)
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, green)
+    glRotate(yrot, 0, 1, 0)
+    glRotate(xrot, 0, 1, 0)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, green)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white)
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 0)
     glutSolidTeapot(0.3)
-    glTranslatef(1, 0, 0)
+    glEnable(GL_DEPTH_TEST)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glTranslatef(0.5, 0, 0)
+    glRotate(yrot, 0, 1, 0)
+    glRotate(xrot, 0, 1, 0)
     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, red)
-    glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, 6)
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, white)
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 8)
     glutSolidTeapot(0.3)
-    glTranslatef(-0.5, 0, 0)
-
     glFlush()
     glutSwapBuffers()
 
 
 def main():
+    f()
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
     glutInitWindowSize(700, 700)
     glutInitWindowPosition(50, 50)
     glutCreateWindow("teapots")
     glutDisplayFunc(show)
+    glutIdleFunc(show)
     glutSpecialFunc(rotate)
     light()
     glutMainLoop()
